96802702ce9e3fead10bd0ee4e0911a62bd5360b
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-08-01 16:02:06 -0300

    add hello-world

--- hello-world/src/main.rs
+++ hello-world/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
+    greet_world();
+}
+
+fn greet_world() {
     println!("Hello, world!");
+    let southern_germany = "Grüß Gott!";
+    let japan = "ハロー・ワールド";
+    let brazil = "Olá, mundo!";
+    let regions = [southern_germany, japan, brazil];
+    for region in regions.iter() {
+        println!("{}", &region);
+    }
 }
