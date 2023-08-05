1f0163514520cce0768bc05b4a03667fd6a962b8
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-08-05 00:58:55 -0300

    add read from files feature

--- chapter_2/grep-lite/src/main.rs
+++ chapter_2/grep-lite/src/main.rs
@@ -1,15 +1,38 @@
+use clap::{App, Arg};
 use regex::Regex;
+use std::fs::File;
+//use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
 // simple program that search a term in a quote and return the line.
 fn main() {
-    let re = Regex::new("picture").unwrap();
+    let args = App::new("grep-lite")
+        .version("0.1")
+        .about("searches for patterns")
+        .arg(
+            Arg::with_name("pattern")
+                .help("The pattern to search for")
+                .takes_value(true)
+                .required(true),
+        )
+        .arg(
+            Arg::with_name("input")
+                .help("File to search")
+                .takes_value(true)
+                .required(true),
+        )
+        .get_matches();
 
-    let quote = "Every face, every shop, bedroom window, public-house, and
-dark square is a picture feverishly turned--in search of what?
-It is the same with books. What do we seek through millions of pages?";
+    let pattern = args.value_of("pattern").unwrap();
+    let re = Regex::new(pattern).unwrap();
 
-    for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
+    let input = args.value_of("input").unwrap();
+    let f = File::open(input).unwrap();
+    let reader = BufReader::new(f);
+
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
             Some(_) => println!("{}", line),
             None => (),
         }
