95c628e53a45050a060cd89c606e80fe15ca48a9
Thiago Costa <86612543+tjthiagocosta@users.noreply.github.com>
2023-08-05 00:29:00 -0300

    add regex support

--- chapter_2/grep-lite/src/main.rs
+++ chapter_2/grep-lite/src/main.rs
@@ -1,15 +1,17 @@
+use regex::Regex;
 // simple program that search a term in a quote and return the line.
 fn main() {
-    let search_term = "book";
-    let quote = "\
-Every face, every shop, bedroom window, public-house, and
+    let re = Regex::new("picture").unwrap();
+
+    let quote = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
 
-    for (i, line) in quote.lines().enumerate() {
-        if line.contains(search_term) {
-            let line_num = i + 1;
-            println!("{}: {}", line_num, line)
+    for line in quote.lines() {
+        let contains_substring = re.find(line);
+        match contains_substring {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
